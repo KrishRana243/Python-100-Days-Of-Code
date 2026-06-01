a59c93c470f1808b994905974967035cb164d169
Krish Rana <krishranaxi@gmail.com>
2026-06-01 10:56:32 +0530

    Add files via upload

---
+++ Intermediate+/Day47/static.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+
+MY_EMAIL = "krish7614.codes@gmail.com"
+MY_PASSWORD = "cbvf xoot udoy xqcf"
+
+# Practice
+url = "https://appbrewery.github.io/instant_pot/"
+# Live Site
+# url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+# print(soup.prettify())
+
+# Find the HTML element that contains the price
+price = soup.find(class_="a-offscreen").get_text()
+
+# Remove the dollar sign using split
+price_without_currency = price.split("$")[1]
+
+# Convert to floating point number
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+# ====================== Send an Email ===========================
+
+# Get the product title
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+# Set the price below which you would like to get a notification
+BUY_PRICE = 100
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is on sale for {price}!"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        result = connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="krishranxi@gmail.com",
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
